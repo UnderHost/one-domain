5b6bc2406a8306548ff6903f1e3387cfbe87d9b0
UnderHost <msa@underhost.com>
2023-04-09 23:52:47 -0700

    Update uh.sh

--- uh.sh
+++ uh.sh
@@ -1,79 +1,6 @@
 #!/bin/bash
 
-# Ensure the script is run as root
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
-# Detect OS
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    OS=$NAME
-else
-    echo "Unable to detect OS"
-    exit 1
-fi
-
-# Update system
-if [ "$OS" == "CentOS Linux" ]; then
-    yum update -y
-elif [ "$OS" == "Ubuntu" ] || [ "$OS" == "Debian GNU/Linux" ]; then
-    apt-get update && apt-get upgrade -y
-fi
-
-# 1. Firewall
-if [ "$OS" == "CentOS Linux" ]; then
-    yum install -y firewalld
-    systemctl start firewalld
-    systemctl enable firewalld
-elif [ "$OS" == "Ubuntu" ] || [ "$OS" == "Debian GNU/Linux" ]; then
-    apt-get install -y ufw
-    ufw enable
-fi
-
-# 2. Close unnecessary ports
-if [ "$OS" == "CentOS Linux" ]; then
-    firewall-cmd --permanent --add-service=http
-    firewall-cmd --permanent --add-service=https
-    firewall-cmd --permanent --remove-service=smtp
-    firewall-cmd --permanent --remove-service=smtps
-    firewall-cmd --permanent --remove-service=pop3
-    firewall-cmd --permanent --remove-service=pop3s
-    firewall-cmd --permanent --remove-service=imap
-    firewall-cmd --permanent --remove-service=imaps
-    firewall-cmd --permanent --remove-service=telnet
-    firewall-cmd --permanent --remove-service=samba
-    firewall-cmd --reload
-elif [ "$OS" == "Ubuntu" ] || [ "$OS" == "Debian GNU/Linux" ]; then
-    ufw allow http
-    ufw allow https
-    ufw deny smtp
-    ufw deny smtps
-    ufw deny pop3
-    ufw deny pop3s
-    ufw deny imap
-    ufw deny imaps
-    ufw deny telnet
-    ufw deny samba
-fi
-
-# 3. Change default listening SSH port
-read -p "Enter a new SSH port (default 22): " new_ssh_port
-new_ssh_port=${new_ssh_port:-22}
-sed -i "s/#Port 22/Port $new_ssh_port/" /etc/ssh/sshd_config
-systemctl restart sshd
-
-# 4. Malware AV Scanner
-if [ "$OS" == "CentOS Linux" ]; then
-    yum install -y clamav clamav-update
-    freshclam
-elif [ "$OS" == "Ubuntu" ] || [ "$OS" == "Debian GNU/Linux" ]; then
-    apt-get install -y clamav clamav-freshclam
-    freshclam
-fi
-
-#!/bin/bash
+OS=$(awk -F= '/^NAME/{print $2}' /etc/os-release | tr -d '"')
 
 # Ensure the script is run as root
 if [ "$(id -u)" != "0" ]; then
@@ -97,100 +24,103 @@ elif [ "$OS" == "Ubuntu" ] || [ "$OS" == "Debian GNU/Linux" ]; then
     apt-get update && apt-get upgrade -y
 fi
 
-# 1. Firewall
-if [ "$OS" == "CentOS Linux" ]; then
+show_menu() {
+  echo "===================="
+  echo "  Security Menu"
+  echo "===================="
+  echo "1. Change SSH Listening Port"
+  echo "2. Install and Configure Firewall"
+  echo "3. Install and Configure Fail2Ban"
+  echo "4. Install and Configure SpamAssassin"
+  echo "5. Disable IPv6"
+  echo "6. Disable Root Logins"
+  echo "7. Optimize OpenLiteSpeed"
+  echo "q. Quit"
+  echo ""
+  echo "Enter the number of the action you'd like to perform:"
+}
+
+run_change_ssh_port() {
+  read -p "Enter the new SSH port: " new_ssh_port
+  sed -i "s/^#Port 22/Port $new_ssh_port/" /etc/ssh/sshd_config
+  systemctl restart sshd
+  echo "SSH port has been changed to $new_ssh_port. Please update your SSH client settings."
+}
+
+run_install_configure_firewall() {
+  if [[ $OS == "CentOS Linux" ]]; then
     yum install -y firewalld
-    systemctl start firewalld
     systemctl enable firewalld
-elif [ "$OS" == "Ubuntu" ] || [ "$OS" == "Debian GNU/Linux" ]; then
+    systemctl start firewalld
+  elif [[ $OS == "Ubuntu" || $OS == "Debian GNU/Linux" ]]; then
     apt-get install -y ufw
     ufw enable
-fi
+  fi
+  echo "Firewall has been installed and configured."
+}
 
-# 2. Close unnecessary ports
-if [ "$OS" == "CentOS Linux" ]; then
-    firewall-cmd --permanent --add-service=http
-    firewall-cmd --permanent --add-service=https
-    firewall-cmd --permanent --remove-service=smtp
-    firewall-cmd --permanent --remove-service=smtps
-    firewall-cmd --permanent --remove-service=pop3
-    firewall-cmd --permanent --remove-service=pop3s
-    firewall-cmd --permanent --remove-service=imap
-    firewall-cmd --permanent --remove-service=imaps
-    firewall-cmd --permanent --remove-service=telnet
-    firewall-cmd --permanent --remove-service=samba
-    firewall-cmd --reload
-elif [ "$OS" == "Ubuntu" ] || [ "$OS" == "Debian GNU/Linux" ]; then
-    ufw allow http
-    ufw allow https
-    ufw deny smtp
-    ufw deny smtps
-    ufw deny pop3
-    ufw deny pop3s
-    ufw deny imap
-    ufw deny imaps
-    ufw deny telnet
-    ufw deny samba
-fi
-
-# 3. Change default listening SSH port
-read -p "Enter a new SSH port (default 22): " new_ssh_port
-new_ssh_port=${new_ssh_port:-22}
-sed -i "s/#Port 22/Port $new_ssh_port/" /etc/ssh/sshd_config
-systemctl restart sshd
-
-# 4. Malware AV Scanner
-if [ "$OS" == "CentOS Linux" ]; then
-    yum install -y clamav clamav-update
-    freshclam
-elif [ "$OS" == "Ubuntu" ] || [ "$OS" == "Debian GNU/Linux" ]; then
-    apt-get install -y clamav clamav-freshclam
-    freshclam
-fi
-
-# 5. Intrusion Detection Software
-if [ "$OS" == "CentOS Linux" ]; then
+run_install_configure_fail2ban() {
+  if [[ $OS == "CentOS Linux" ]]; then
+    yum install -y epel-release
     yum install -y fail2ban
-    systemctl enable fail2ban
-    systemctl start fail2ban
-elif [ "$OS" == "Ubuntu" ] || [ "$OS" == "Debian GNU/Linux" ]; then
+  elif [[ $OS == "Ubuntu" || $OS == "Debian GNU/Linux" ]]; then
     apt-get install -y fail2ban
-    systemctl enable fail2ban
-    systemctl start fail2ban
-fi
+  fi
+  systemctl enable fail2ban
+  systemctl start fail2ban
+  echo "Fail2Ban has been installed and configured."
+}
 
-# 6. Brute Force Security
-cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
-echo "
-[sshd]
-enabled  = true
-port     = $new_ssh_port
-filter   = sshd
-logpath  = /var/log/auth.log
-maxretry = 3
-bantime  = 3600
-" >> /etc/fail2ban/jail.local
-systemctl restart fail2ban
-
-# 7. Spam Filtering - Install SpamAssassin
-if [ "$OS" == "CentOS Linux" ]; then
+run_install_configure_spamassassin() {
+  if [[ $OS == "CentOS Linux" ]]; then
     yum install -y spamassassin
-    systemctl enable spamassassin
-    systemctl start spamassassin
-elif [ "$OS" == "Ubuntu" ] || [ "$OS" == "Debian GNU/Linux" ]; then
+  elif [[ $OS == "Ubuntu" || $OS == "Debian GNU/Linux" ]]; then
     apt-get install -y spamassassin
-    systemctl enable spamassassin
-    systemctl start spamassassin
-fi
+  fi
+  systemctl enable spamassassin
+  systemctl start spamassassin
+  echo "SpamAssassin has been installed and configured."
+}
 
-# 8. Disable IPV6
-echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
-sysctl -p
+run_disable_ipv6() {
+  echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+  echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+  echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+  sysctl -p
+  echo "IPv6 has been disabled."
+}
 
-# 9. Disable Root Logins
-sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin no/" /etc/ssh/sshd_config
-systemctl restart sshd
+run_disable_root_logins() {
+  sed -i "s/^#PermitRootLogin .*/PermitRootLogin no/" /etc/ssh/sshd_config
+  systemctl restart sshd
+  echo "Root logins have been disabled."
+}
+
+run_optimize_openlitespeed() {
+  echo "Please provide the optimization steps for OpenLiteSpeed."
+}
 
 
-echo "Security and optimization tasks completed."
+while true; do
+  show_menu
+  read -r user_choice
+  case "$user_choice" in
+    1) run_change_ssh_port ;;
+    2) run_install_configure_firewall ;;
+    3) run_install_configure_fail2ban ;;
+    4) run_install_configure_spamassassin ;;
+    5) run_disable_ipv6 ;;
+    6) run_disable_root_logins ;;
+    7) run_optimize_openlitespeed ;;
+    q) break ;;
+    *) echo "Invalid option, please try again." ;;
+  esac
+  echo ""
+done
+
+echo "Exiting the script."
+
+
+
+
+
