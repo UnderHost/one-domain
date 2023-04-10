26596edb771f5f798600f98be2b8056fdf4ea530
UnderHost <msa@underhost.com>
2023-04-09 21:23:53 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Check the operating system and install required packages
 if [[ $(command -v yum) ]]; then
     # CentOS
@@ -8,12 +6,24 @@ if [[ $(command -v yum) ]]; then
     yum -y install nginx mariadb mariadb-server php-fpm php-mysqlnd
     systemctl enable nginx mariadb php-fpm
     systemctl start nginx mariadb php-fpm
+    
+    # Install OpenLiteSpeed and PHP LSAPI
+    yum -y install openlitespeed lsphp80 lsphp80-common lsphp80-mysqlnd lsphp80-gd lsphp80-process lsphp80-xml lsphp80-pdo lsphp80-imap lsphp80-mbstring lsphp80-bcmath lsphp80-json
+    systemctl enable lsws
+    systemctl start lsws
 elif [[ $(command -v apt) ]]; then
     # Debian or Ubuntu
     apt-get update
     apt-get -y install nginx mariadb-server php-fpm php-mysql
     systemctl enable nginx mariadb php-fpm
     systemctl start nginx mariadb php-fpm
+    
+    # Install OpenLiteSpeed and PHP LSAPI
+    wget -O - http://rpms.litespeedtech.com/debian/enable_lst_debain_repo.sh | bash
+    apt-get update
+    apt-get -y install openlitespeed lsphp80 lsphp80-common lsphp80-mysql lsphp80-gd lsphp80-process lsphp80-xml lsphp80-pdo lsphp80-imap lsphp80-mbstring lsphp80-bcmath lsphp80-json
+    systemctl enable lsws
+    systemctl start lsws
 else
     echo "Operating system not supported."
     exit 1
@@ -51,6 +61,29 @@ elif [[ $(command -v apt) ]]; then
     systemctl start php8.0-fpm
 fi
 
+# Install common PHP extensions
+echo "Installing common PHP extensions..."
+if [[ $(command -v yum) ]]; then
+    # CentOS
+    yum -y install php-gd php-xml php-mbstring php-zip php-bcmath php-json php-curl php-opcache
+elif [[ $(command -v apt) ]]; then
+    # Debian or Ubuntu
+    apt-get -y install php-gd php-xml php-mbstring php-zip php-bcmath php-json php-curl php-opcache
+fi
+
+# Install ionCube Loader
+echo "Installing ionCube Loader..."
+php_version=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
+ioncube_url="https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz"
+wget $ioncube_url -O /tmp/ioncube_loaders.tar.gz
+tar -zxvf /tmp/ioncube_loaders.tar.gz -C /tmp/
+ioncube_dir=$(find /tmp/ -name "ioncube_*" -type d)
+cp $ioncube_dir/ioncube_loader_lin_$php_version.so /usr/lib64/php/modules/
+cat << EOF > /etc/php.d/00-ioncube.ini
+zend_extension = /usr/lib64/php/modules/ioncube_loader_lin_$php_version.so
+EOF
+
+
 # Check server specs and optimize Nginx configuration and PHP settings
 cpu_cores=$(grep -c ^processor /proc/cpuinfo)
 ram_mb=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
@@ -65,27 +98,7 @@ sed -i "s/pm.start_servers = .*/pm.start_servers = $(($max_worker_processes/2))/
 sed -i "s/pm.min_spare_servers = .*/pm.min_spare_servers = $(($max_worker_processes/4))/" /etc/php-fpm.d/www.conf
 sed -i "s/pm.max_spare_servers = .*/pm.max_spare_servers = $(($max_worker_processes/2))/" /etc/php-fpm.d/www.conf
 
-# Install ionCube Loader
-echo "Installing ionCube Loader..."
-php_version=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
-ioncube_url="https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz"
-wget $ioncube_url -O /tmp/ioncube_loaders.tar.gz
-tar -zxvf /tmp/ioncube_loaders.tar.gz -C /tmp/
-ioncube_dir=$(find /tmp/ -name "ioncube_*" -type d)
-cp $ioncube_dir/ioncube_loader_lin_$php_version.so /usr/lib64/php/modules/
-cat << EOF > /etc/php.d/00-ioncube.ini
-zend_extension = /usr/lib64/php/modules/ioncube_loader_lin_$php_version.so
-EOF
 
-# Install common PHP extensions
-echo "Installing common PHP extensions..."
-if [[ $(command -v yum) ]]; then
-    # CentOS
-    yum -y install php-gd php-xml php-mbstring php-zip php-bcmath php-json php-curl php-opcache
-elif [[ $(command -v apt) ]]; then
-    # Debian or Ubuntu
-    apt-get -y install php-gd php-xml php-mbstring php-zip php-bcmath php-json php-curl php-opcache
-fi
 
 # Optimize PHP configuration
 echo "Optimizing PHP configuration..."
@@ -130,6 +143,10 @@ mkdir -p /var/www/$main_domain/html
 chown -R $ftp_user:$ftp_user /var/www/$main_domain/html
 echo -e "$ftp_user\n$ftp_password" >> /etc/vsftpd/virtual_users.txt
 db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
+
+
+# Create PHP info file
+echo "<?php phpinfo(); ?>" > /var/www/$main_domain/html/info.php
 
 # Get path to public_html directory
 public_html=$(grep "^$main_domain:" /etc/userdomains | awk '{print "/home/"$2"/public_html"}')
