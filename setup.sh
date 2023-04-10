92fe6f8430e26ed1cb41e7f635f1ae3aaa45ad75
UnderHost <msa@underhost.com>
2023-04-09 21:03:27 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -51,21 +51,28 @@ elif [[ $(command -v apt) ]]; then
     systemctl start php8.0-fpm
 fi
 
-# Check server specs and optimize Nginx configuration and PHP settings
+Check server specs and optimize Nginx configuration and PHP settings
 cpu_cores=$(grep -c ^processor /proc/cpuinfo)
 ram_mb=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
-max_worker_processes=$((cpu_cores*2))
+max_worker_processes=$((cpu_cores2))
 sed -i "s/worker_processes auto/worker_processes $max_worker_processes/g" /etc/nginx/nginx.conf
-sed -i "s/worker_connections 1024/worker_connections $(($cpu_cores*1024))/g" /etc/nginx/nginx.conf
+sed -i "s/worker_connections 1024/worker_connections $(($cpu_cores1024))/g" /etc/nginx/nginx.conf
 sed -i "s/keepalive_timeout 65/keepalive_timeout 5/g" /etc/nginx/nginx.conf
 sed -i "s/^# server_tokens off;/server_tokens off;/g" /etc/nginx/nginx.conf
-sed -i "s/memory_limit = .*/memory_limit = $(($ram_mb/8))M/" /etc/php-fpm.d/www.conf
-sed -i "s/pm.max_children = .*/pm.max_children = $max_worker_processes/" /etc/php-fpm.d/www.conf
-sed -i "s/pm.start_servers = .*/pm.start_servers = $(($max_worker_processes/2))/" /etc/php-fpm.d/www.conf
-sed -i "s/pm.min_spare_servers = .*/pm.min_spare_servers = $(($max_worker_processes/4))/" /etc/php-fpm.d/www.conf
+sed -i "s/memory_limit = ./memory_limit = $(($ram_mb/8))M/" /etc/php-fpm.d/www.conf
+sed -i "s/pm.max_children = ./pm.max_children = $max_worker_processes/" /etc/php-fpm.d/www.conf
+sed -i "s/pm.start_servers = ./pm.start_servers = $(($max_worker_processes/2))/" /etc/php-fpm.d/www.conf
+sed -i "s/pm.min_spare_servers = ./pm.min_spare_servers = $(($max_worker_processes/4))/" /etc/php-fpm.d/www.conf
 sed -i "s/pm.max_spare_servers = .*/pm.max_spare_servers = $(($max_worker_processes/2))/" /etc/php-fpm.d/www.conf
 
-# Create main domain and database
+Optimize MySQL settings
+mysql_tuning_primer_url='https://raw.githubusercontent.com/major/MySQLTuner-perl/master/basic_passwords.txt'
+wget $mysql_tuning_primer_url -O /usr/local/sbin/tuning-primer.sh
+chmod +x /usr/local/sbin/tuning-primer.sh
+/usr/local/sbin/tuning-primer.sh > /etc/mysql/my.cnf
+service mysql restart
+
+Create main domain and database
 echo "Enter main domain:"
 read main_domain
 echo "Enter MySQL root password:"
@@ -78,7 +85,7 @@ read -s mysql_user_password
 mysql -u root -p$mysql_root_password -e "CREATE USER '$mysql_user'@'localhost' IDENTIFIED BY '$mysql_user_password'"
 mysql -u root -p$mysql_root_password -e "GRANT ALL PRIVILEGES ON $main_domain.* TO '$mysql_user'@'localhost'"
 
-# Create FTP user and directory for main domain
+Create FTP user and directory for main domain
 echo "Enter FTP username for $main_domain:"
 read ftp_user
 echo "Enter password for $ftp_user:"
@@ -88,11 +95,8 @@ chown -R $ftp_user:$ftp_user /var/www/$main_domain/html
 echo -e "$ftp_user\n$ftp_password" >> /etc/vsftpd/virtual_users.txt
 db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
 
-# Get path to public_html directory
+Get path to public_html directory
 public_html=$(grep "^$main_domain:" /etc/userdomains | awk '{print "/home/"$2"/public_html"}')
 
-# Create text file with login information
-echo -e "Domain: $main_domain\nPublic HTML Directory: /var/www/$main_domain/html\nMySQL User: $mysql_user\nMySQL Password: $mysql_user_password\nFTP User: $ftp_user\nFTP Password: $ftp_password" > /root/login_info.txt
-
-echo "LEMP setup and domain configuration completed successfully. Login information saved to /root/login_info.txt."
-
+Create text file with login information
+echo -e "Domain: $main_domain\nPublic HTML Directory: /var/www/$main_domain/html\nMySQL User: $mysql_user\nMySQL Password: $mysql_user_password
